b1a7f3ab1f1ebb0c0454f64cd3ce468571fa2353
Vanesa Honorato <vanegoho@gmail.com>
2025-10-13 14:49:56 -0600

    repositorio

---
+++ UNIDAD2_SERIES_FOURIER/hola.py
@@ -0,0 +1,28 @@
+"""
+FECHA:09/10/25
+NOMBRE: Vanesa Gonzalez Honorato
+MATERIA: Analisis de señales y comunicacion
+ITESS TICS 501
+vanegoho@gmail.com
+"""
+print("hola mundo");
+#suma
+print("Suma: 2+2 = ", 2+2)
+#resta
+print("Resta: 2-2 = ", 2-2)
+#Multiplicacion
+print("Multiplicacion: 2*2 =", 2*2)
+#Division
+print("Division 10/3 =", 10/3)
+#division doble
+print("Division doble 10//3 =", 10//3)
+#Modulo
+print("Modulo: 10%3 =", 10%3)
+#Exponencial
+print("Exponencial: 2**3 =", 2**3)
+
+nombre = "Vanesa"
+edad = 20
+altura = 1.60
+print("Me llamo", nombre, "y tengo", edad, "años.")
+
