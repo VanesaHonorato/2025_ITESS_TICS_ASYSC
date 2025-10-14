8cc362004110b4085a9f3a88f01e37abbf1250ca
Vanesa Honorato <vanegoho@gmail.com>
2025-10-13 20:40:55 -0600

    EJERCICIOS PRIMEROS PASOS HACIA LA PROGRAMACION

---
+++ UNIDAD2_SERIES_FOURIER/PrimerosPasosHaciaLaProgramacion.py
@@ -0,0 +1,24 @@
+
+print("\nSerie de Fibonacci la suma de dos elementos define el siguiente")
+a, b = 1, 1  
+while b < 20:  
+    print('Número:', b)
+    a, b = b, a + b
+
+print("\nUso de print() con varios valores")
+x = 25 * 25
+print('El resultado de 25 x 25 es', x)
+
+print("\nSerie de Fibonacci hasta 500 en una sola línea")
+a, b = 0, 1
+while b < 500:
+    print(b, end=' | ')  
+    a, b = b, a + b
+
+
+print("\nPrueba de potencias")
+print(-4**2)     
+print((-4)**2)   
+
+
+
